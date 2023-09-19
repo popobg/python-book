47cedfd50c44b405a75f76a4e4e6969335527bb1
popobg <pauline.bouyssou1@gmail.com>
2023-09-19 19:30:11 +0200

    Change choices to dictionnary

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,45 +2,36 @@
 
 import random
 
-p = "pierre"
-f = "feuille"
-c = "ciseaux"
-choices = [p, f, c]
+choices = {
+    "p": "pierre",
+    "f": "feuille",
+    "c": "ciseaux"
+}
 
-computer_choice = random.choice(choices)
+computer_choice = random.choice(list(choices.keys()))
 
-prompt = "pierre (p), feuille (f), ciseaux (c) ?\n> "
-
-
-def controlled_input(prompt):
+def controlled_input():
     while True:
-        user_input = input(prompt).lower()
+        user_input = input("pierre (p), feuille (f), ciseau (c) ?\n> ").lower()
         if user_input == "" \
-        or len(user_input) > 1 and user_input not in choices \
-        or len(user_input) == 1 and user_input not in ["p", "c", "f"]:
+        or len(user_input) > 1 and user_input not in choices.values() \
+        or len(user_input) == 1 and user_input not in choices.keys():
             print("Terme non reconnu. Recommencez.")
             continue
-        if len(user_input) == 1:
-            if user_input == "p":
-                return p
-            if user_input == "f":
-                return f
-            if user_input == "c":
-                return c
-        return user_input
+        return user_input[0]
 
-player_choice = controlled_input(prompt)
+player_choice = controlled_input()
 
 while player_choice == computer_choice:
-    print(f"Egalité ! Vous avez joué tous les deux joué {computer_choice}. Recommençons.")
-    computer_choice = random.choice(choices)
-    player_choice = controlled_input(prompt)
+    print(f"Egalité ! Vous avez tous les deux joué {choices[computer_choice]}. Recommençons.")
+    computer_choice = random.choice(list(choices.keys()))
+    player_choice = controlled_input()
 
 # Attention à ne pas créer de variable trop tôt et qu'elle ne s'actualise pas.
 
-if player_choice == p and computer_choice == c \
-or player_choice == c and computer_choice == f \
-or player_choice == f and computer_choice == p:
-    print(f"Votre adversaire a joué {computer_choice}. Vous avez gagné !")
+if player_choice == "p" and computer_choice == "c" \
+or player_choice == "c" and computer_choice == "f" \
+or player_choice == "f" and computer_choice == "p":
+    print(f"Votre adversaire a joué {choices[computer_choice]}. Vous avez gagné !")
 else:
-    print(f"Votre adversaire a joué {computer_choice}. C'est perdu...")
+    print(f"Votre adversaire a joué {choices[computer_choice]}. C'est perdu...")
