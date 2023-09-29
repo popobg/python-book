83f80baafab4f584ae027439f75d6653a3f42fca
popobg <pauline.bouyssou1@gmail.com>
2023-09-29 17:58:47 +0200

    Update caesar cipher

--- caesar_cipher/CaesarCipher_slicingversion.py
+++ caesar_cipher/CaesarCipher_slicingversion.py
@@ -1,12 +1,8 @@
 #! /usr/bin/env python3
 
-import time
-
 """The purpose of this code is to decypher an encode message \
 or to encode a message.
 A letter matchs the letter + 6 of the alphabet."""
-
-start_time = time.time()
 
 # VARIABLES
 
@@ -23,17 +19,16 @@ str6 = "Ans: uqxg oy znk iruykyz znotm zu terut o'bk kbkx kgzkt."
 # FUNCTIONS
 
 def encode(letter,key):
-    if letter in alphabet:
-        letter = alphabet.index(letter)
-        if key > 0 and letter in range((len(alphabet) - key), len(alphabet)):
-            letter = alphabet[letter - (len(alphabet) - key)]
-        elif key < 0 and letter in range(0, key):
-            letter = alphabet[letter + (len(alphabet) - key)]
-        else:
-            letter = alphabet[letter + key]
+    if letter not in alphabet:
         return letter
+    letter = alphabet.index(letter)
+    if key > 0 and letter in range((len(alphabet) - key), len(alphabet)):
+        letter = alphabet[letter - (len(alphabet) - key)]
+    elif key < 0 and letter in range(0, key):
+        letter = alphabet[letter + (len(alphabet) - key)]
     else:
-        return letter
+        letter = alphabet[letter + key]
+    return letter
 
 def decode(letter, key):
     return encode(letter, key * -1)
@@ -54,6 +49,3 @@ for i in str2.lower():
     i = decode(i, 8)
     print(i, end = "")
 print()
-
-end_time = time.time()
-print((end_time - start_time) * 1000)
