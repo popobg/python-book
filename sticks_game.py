459a54be5a7340fc28221072ba98a5a3cdb8ec22
popobg <pauline.bouyssou1@gmail.com>
2023-10-06 09:19:40 +0200

    Add sticks_game

---
+++ sticks_game.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+
+import popo_tools, random, time
+
+def remove_sticks(choice, sticks_number):
+    sticks_number = sticks_number - choice
+    return sticks_number
+
+def player_turn(sticks_number):
+        upper_limit = 3 if sticks_number >= 3 else sticks_number
+        player_move = popo_tools.input_int(player_prompt, [1, upper_limit])
+        sticks_number = remove_sticks(player_move, sticks_number)
+        return sticks_number
+
+def computer_turn(sticks_number):
+    computer_move = random.choices(computer_choices)[0]
+    print(f"The computer choose {computer_move} {make_stick_words(computer_move)}.")
+    sticks_number = remove_sticks(computer_move, sticks_number)
+    return sticks_number
+
+def make_stick_words(sticks_number):
+    return "stick" if sticks_number == 1 else "sticks"
+
+player_prompt = "It is your turn: "
+sticks_number = 21
+computer_choices = [1, 2, 3]
+
+print("There are 21 sticks. You can remove between 1 and 3 sticks at each round.")
+print("The winner is the one that removes the last stick.")
+
+while True:
+    if sticks_number > 1:
+        sticks_number = player_turn(sticks_number)
+        print(f"{sticks_number} {make_stick_words(sticks_number)} remaining.")
+        if sticks_number == 0:
+            print("Congratulations, you win!")
+            exit()
+    else:
+        print("It is your turn, you win.")
+        exit()
+    time.sleep(1)
+    if sticks_number <= 3:
+        print(f"The computer choose 3 sticks, you lose.")
+        exit()
+    else:
+        sticks_number = computer_turn(sticks_number)
+        print(f"{sticks_number} {make_stick_words(sticks_number)} remaining.")
