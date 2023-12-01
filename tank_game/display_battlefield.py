32d5bf699dd40fb8bfd5aca56c3b6105092636ad
popobg <pauline.bouyssou1@gmail.com>
2023-12-01 10:52:10 +0100

    add function's descriptions and a function to approximate the last x position

--- tank_game/display_battlefield.py
+++ tank_game/display_battlefield.py
@@ -38,6 +38,7 @@ class Sprite:
 
 class Cannon(Sprite):
     def __init__(self, x, y):
+        # call the method init of the superclass Sprite to initialize the attributes of the subclass Cannon
         Sprite.__init__(self, x, y, "/")
 
 class Target(Sprite):
@@ -49,6 +50,7 @@ class Cannonball(Sprite):
         Sprite.__init__(self, x, y, "o")
 
     def update_position(self, dx, dy):
+        """update the position of the moving object by adding the acceleration vectors dx and dy"""
         x = Sprite.get_x(self)
         x += dx
         Sprite.set_x(self, round(x))
@@ -80,13 +82,14 @@ class Display:
         self.__sprites.pop()
 
     def __init_display__(self):
+        """create a list of lists defining the range of the field"""
         self.__field = []
         for i in range(self.__height):
             self.__field.append([" "] * self.__width)
         return self
 
     def draw_battlefield(self):
-        # create the list of lists for the battlefield
+        """draw battlefield by printing the field and sprites on the terminal"""
         self.__init_display__()
         # replace the blank by the objects's sprite at the right position in the field
         for sprite in self.__sprites:
@@ -104,13 +107,16 @@ def clear():
 def wait():
     time.sleep(1)
 
-def calculate_move(d: Display):
+def calculate_moving_vectors(d: Display):
     dy = 0.1 * d.get_height()
     dx = 0.1 * d.get_width()
     return dx, dy
 
-def applicate_friction(dy, initial_dy):
-    return (dy - initial_dy)
+def applicate_friction(dy, initial_dy, dx, initial_dx):
+    """update dx and dy to apply deceleration on the ball"""
+    dy = dy - initial_dy
+    dx = dx * 0.8
+    return dy, dx
 
 def update_display(d: Display):
     clear()
@@ -118,17 +124,28 @@ def update_display(d: Display):
     wait()
 
 def ball_out_of_field(s: Sprite, d: Display):
-    if s.get_y() < 0:
+    """return True if the ball is out of the range of the field or on the ground
+    otherwise return False"""
+    if s.get_y() <= 0:
         return True
     if s.get_y() > d.get_height() or s.get_x() > d.get_width():
         return True
     return False
 
+def determine_x(s: Sprite, initial_dx, initial_dy):
+    """approximate the position of x when the ball touch the ground (y = 0)"""
+    s_y = s.get_y()
+    s_x = s.get_x()
+    while round(s_y) < 0:
+        s_y += 0.1 * initial_dy
+        s_x += 0.1 * initial_dx
+    return round(s_x)
+
 # MAIN
 
 cannon = Cannon(12, 0)
 target = Target(60, 0)
-ball = Cannonball(14, 2)
+ball = Cannonball(cannon.get_x() + 1, cannon.get_y() + 1)
 
 max_x = max(cannon.get_x(), target.get_x(), ball.get_x())
 max_y = max(cannon.get_y(), target.get_y(), ball.get_y())
@@ -139,21 +156,26 @@ battlefield.add_sprite(cannon)
 battlefield.add_sprite(target)
 battlefield.add_sprite(ball)
 
-dx, dy = calculate_move(battlefield)
+dx, dy = calculate_moving_vectors(battlefield)
 initial_dy = dy
+initial_dx = dx
+update_position = 0
 
 while not ball_out_of_field(ball, battlefield):
     update_display(battlefield)
     ball.update_position(dx, dy)
-    dy = applicate_friction(dy, initial_dy)
+    update_position += 1
+    if update_position > 2 :
+        dy, dx = applicate_friction(dy, initial_dy, dx, initial_dx)
 
 # ball is on the ground or out of sight
 # remove it from de sprites of the field
 battlefield.remove_sprite(ball)
 
-if ball.get_y() < 0:
+if ball.get_y() <= 0:
+    x_impact = determine_x(ball, initial_dx, initial_dy)
     # create an instance of the class impact
-    impact = Impact(ball.get_x(), 0)
+    impact = Impact(x_impact, 0)
     # add a new sprite to the field
     battlefield.add_sprite(impact)
     update_display(battlefield)
